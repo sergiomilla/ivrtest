8d920b35c91d2e7909db4b687c2d812fe3aa057a
sergiomilla <sergio.milla@servexternos.isban.es>
2013-06-19 14:57:04 +0200

    Bug1 solucionado

--- ProjectTest/src/com/isb/test/Main.java
+++ ProjectTest/src/com/isb/test/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba.");
-
+		//Añado esto para solucionar el bug1
+		System.out.println("Solucionado");
 	}
 
 }
