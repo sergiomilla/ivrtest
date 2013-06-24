f13f619bb6b1bdcfb8d2e81bb7933ea3bc9b2de0
Idafe Reyes <idafe.reyes@servexternos.isban.es>
2013-06-24 12:41:55 +0200

    Reorganizamos getters y setters.

--- ProjectTest/src/com/isb/test/Alumno.java
+++ ProjectTest/src/com/isb/test/Alumno.java
@@ -3,12 +3,15 @@ package com.isb.test;
 public class Alumno {
 
 	public String nombre;
+
+	public String apellido;
+	
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String apellido;
 	
 }
