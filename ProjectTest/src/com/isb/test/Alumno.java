b606dfa629f479338c27cc6bc711d398e6c516ae
Idafe Reyes <idafe.reyes@servexternos.isban.es>
2013-06-24 12:34:06 +0200

    Getters y setters de clase alumno.

--- ProjectTest/src/com/isb/test/Alumno.java
+++ ProjectTest/src/com/isb/test/Alumno.java
@@ -3,6 +3,12 @@ package com.isb.test;
 public class Alumno {
 
 	public String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 	public String apellido;
 	
 }
