71b30a3875ba30eb8a831d7f2ce72420965ff407
Idafe Reyes <idafe.reyes@servexternos.isban.es>
2013-06-25 11:32:05 +0200

    Se cambia la ruta del fichero de cabecera.

--- POCsVXML/src/com/santander/Constants.java
+++ POCsVXML/src/com/santander/Constants.java
@@ -3,7 +3,7 @@ package com.santander;
 public class Constants {
 	
 	public static final int TAM_BUFFER = 1024;
-	public static final String FILE_PATH = "C:/IVRLabs/";
+	public static final String FILE_PATH = "C:/DATOS/IVRLabs/";
 	public static final String HEADER_WAV_FILE = "CabeceraWav.bin";
 	
 }
