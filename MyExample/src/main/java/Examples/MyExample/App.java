c25179df9d21e527ae8dd4f83421480931002149
Idafe Reyes <idafe.reyes@servexternos.isban.es>
2013-07-10 10:43:21 +0200

    Cambio para probar build trigger.

--- MyExample/src/main/java/Examples/MyExample/App.java
+++ MyExample/src/main/java/Examples/MyExample/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helloooo World! Changed. Again... For WebHook URLs" );
+        System.out.println( "Helloooo World! Changed. Again... For WebHook URLs." );
+        System.out.println( "build trigger" );
     }
 }
