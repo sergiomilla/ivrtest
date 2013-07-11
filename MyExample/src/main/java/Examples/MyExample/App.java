1f339b4053f09781aac84ab78cfb59a1c9792369
idafereyes <idafe.reyes@servexternos.isban.es>
2013-07-11 09:29:55 +0200

    Prueba bat 3.

--- MyExample/src/main/java/Examples/MyExample/App.java
+++ MyExample/src/main/java/Examples/MyExample/App.java
@@ -11,6 +11,6 @@ public class App
         System.out.println( "Helloooo World! Changed. Again... For WebHook URLs." );
         System.out.println( "build trigger" );
         System.out.println("Probar hook");
-        System.out.println("Prueba bat2");
+        System.out.println("Prueba bat3");
     }
 }
