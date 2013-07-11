c2203ddb9ebaa35f20d7936e118c0913ad527047
idafereyes <idafe.reyes@servexternos.isban.es>
2013-07-11 09:00:18 +0200

    Probar hook.

--- MyExample/src/main/java/Examples/MyExample/App.java
+++ MyExample/src/main/java/Examples/MyExample/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Helloooo World! Changed. Again... For WebHook URLs." );
         System.out.println( "build trigger" );
+        System.out.println("Probar hook");
     }
 }
